4fd48965ecdf49c2de5e5117cfee4d847e10ee8d
mathew_ovsiannikov <v.pg.98@mail.ru>
2021-08-24 13:42:32 +0300

    add error-boundry, error-button, error-indicator, hoc-helpers,row, sw-components

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,39 +2,94 @@ import React, { Component } from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
+import ErrorBoundry from '../error-boundry';
+
+import ItemDetails, { Record } from "../item-details/item-details";
+import SwapiService from "../../services/swapi-service";
+
+import {
+  PersonDetails,
+  PlanetDetails,
+  StarshipDetails,
+  PersonList,
+  PlanetList,
+  StarshipList
+} from '../sw-components';
 
 import './app.css';
 
 export default class App extends Component {
 
+  swapiService = new SwapiService();
+
   state = {
-    selectedPerson: 5
-  }
+    showRandomPlanet: true
+  };
 
-  onPersonSelected = (id) => {
-    this.setState({
-      selectedPerson: id
-    })
-  }
+  toggleRandomPlanet = () => {
+    this.setState((state) => {
+      return {
+        showRandomPlanet: !state.showRandomPlanet
+      }
+    });
+  };
 
-  render (){
-    return(
-    <div>
-      <Header />
-      <RandomPlanet />
+  render() {
 
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList onItemSelected = {this.onPersonSelected} />
+    const planet = this.state.showRandomPlanet ?
+      <RandomPlanet/> :
+      null;
+
+    const { getPerson,
+            getStarship,
+            getPersonImage,
+            getStarshipImage,
+            getAllPeople,
+            getAllPlanets } = this.swapiService;
+
+    const personDetails = (
+      <ItemDetails
+        itemId={11}
+        getData={getPerson}
+        getImageUrl={getPersonImage} >
+
+        <Record field="gender" label="Gender" />
+        <Record field="eyeColor" label="Eye Color" />
+
+      </ItemDetails>
+    );
+
+    const starshipDetails = (
+      <ItemDetails
+        itemId={5}
+        getData={getStarship}
+        getImageUrl={getStarshipImage}>
+
+        <Record field="model" label="Model" />
+        <Record field="length" label="Length" />
+        <Record field="costInCredits" label="Cost" />
+      </ItemDetails>
+    );
+
+    return (
+      <ErrorBoundry>
+        <div className="stardb-app">
+          <Header />
+
+          <PersonDetails itemId={11} />
+
+          <PlanetDetails itemId={5} />
+
+          <StarshipDetails itemId={9} />
+
+          <PersonList />
+
+          <StarshipList />
+
+          <PlanetList />
+
         </div>
-        <div className="col-md-6">
-          <PersonDetails personId ={this.state.selectedPerson}/>
-        </div>
-      </div>
-    </div>
-  )}
-};
-
-
+      </ErrorBoundry>
+    );
+  }
+}
--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -4,14 +4,12 @@ import './spinner.css';
 
 const Spinner = () => {
   return (
-    <center>
     <div className="lds-css">
       <div className="lds-double-ring">
         <div></div>
         <div></div>
       </div>
     </div>
-    </center>
   );
 };
 
